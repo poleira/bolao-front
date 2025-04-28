4c3618616c7023f1b62323c5715bd46e2bd35712
Marcelo Faria <marcelofariam07@gmail.com>
2025-04-28 20:01:27 -0300

    Remove firebase.config.ts from tracking and add to .gitignore

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDn1njR6kp21z0QMgdxgJzQyolvIZNr8cY",
-    authDomain: "generic-application-2275a.firebaseapp.com",
-    projectId: "generic-application-2275a",
-    storageBucket: "generic-application-2275a.appspot.com",
-    messagingSenderId: "539542416789",
-    appId: "1:539542416789:web:ba849abc80fed2527d9a9c"
-};
