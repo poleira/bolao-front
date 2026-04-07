a29b32b601b48e4ab00708acdc93562525898c8d
Marco Polo Perciano <poleira10@gmail.com>
2026-04-07 00:03:44 -0300

    Tira config do gitignore

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBz5TCgmWWFZOja2ENWYOSbWDUqcjqfiDM",
+    authDomain: "bolao-33185.firebaseapp.com",
+    projectId: "bolao-33185",
+    storageBucket: "bolao-33185.firebasestorage.app",
+    messagingSenderId: "731799507318",
+    appId: "1:731799507318:web:34a2d9f0bb3d8adc39ea57"
+};
